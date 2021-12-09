a25c9d59871a3f78f448da3ce90e4c745b81515b
antonkapakly <kapakly0@gmail.com>
2021-12-09 21:56:36 +0300

    fix lab01

--- Лабораторная 1/Lab1.java
+++ Лабораторная 1/Lab1.java
@@ -10,16 +10,22 @@ public class Lab1 {
         int p2 = 0;
         int count = 3;
         int t;
+        boolean key;
         while (n != N) {
+            key = true;
             for (int j = 3; j < count; j += 2)
-                if (count % j == 0)
+                if (count % j == 0) {
+                    key = false;
                     break;
-            t = p2;
-            p2 = count;
-            p1 = t;
-            if (p2 - p1 == 2) {
-                n++;
-                System.out.println(p1 + " " + p2);
+                }
+            if (key) {
+                t = p2;
+                p2 = count;
+                p1 = t;
+                if (p2 - p1 == 2) {
+                    n++;
+                    System.out.println(p1 + " " + p2);
+                }
             }
             count += 2;
         }
