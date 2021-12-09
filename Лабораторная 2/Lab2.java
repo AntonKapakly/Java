4c1b0694b14c577b7da7b90f3513224f8a538e9f
antonkapakly <kapakly0@gmail.com>
2021-12-09 20:44:33 +0300

    update lab02

--- Лабораторная 2/Lab2.java
+++ Лабораторная 2/Lab2.java
@@ -1,21 +1,23 @@
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class Lab2 {
+
+    public static int rightBorder = 99;
+    public static int leftBorder = -99;
 
     public static int[][] createArray (int N, int M) {
         int[][] array = new int[M][N];
         for (int i = 0; i < M; i++)
             for (int j = 0; j < N; j++)
-                array[i][j] = (int) (Math.random()*(99*2 + 1)) - 99;
+                array[i][j] = (int) (Math.random()*(rightBorder-leftBorder + 1)) + leftBorder;
         return array;
     }
 
-    public static int maxCol (int[][] array, int N, int M) {
-        int maxItem = -100;
+    public static int maxCol (int[][] array) {
+        int maxItem = leftBorder - 1;
         int maxColumn = 0;
-        for (int i = 0; i < M; i++)
-            for (int j = 0; j < N; j++)
+        for (int i = 0; i < array.length; i++)
+            for (int j = 0; j < array[i].length; j++)
                 if (array[i][j] > maxItem) {
                     maxItem = array[i][j];
                     maxColumn = j;
@@ -23,18 +25,27 @@ public class Lab2 {
         return maxColumn;
     }
 
-    public static int[][] delMaxCol (int N, int M, int maxColumn, int[][] array) {
-        int[][] newArray = new int[M][N-1];
-        for (int i = 0; i < M; i++)
-            for (int j = 0; j < N; j++) {
-                if (j == maxColumn)
+    public static int[][] delMaxCol (int[][] array, int maxColumn) {
+        int key = 0;
+        int[][] newArray = new int[array.length][array[0].length-1];
+        for (int i = 0; i < array.length; i++)
+            for (int j = 0; j < array[i].length; j++) {
+                if (j == maxColumn) {
+                    key = 1;
                     continue;
-                if (j > maxColumn)
-                    newArray[i][j-1] = array[i][j];
-                else
-                    newArray[i][j] = array[i][j];
+                }
+                newArray[i][j-key] = array[i][j];
             }
         return newArray;
+    }
+
+    public static void printArray(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++){
+                System.out.printf("%10d", array[i][j]);
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
@@ -45,9 +56,11 @@ public class Lab2 {
         System.out.print("Введите кол-во столбцов N: ");
         int N = n.nextInt();
         int[][] array = createArray(N, M);
-        System.out.println(Arrays.deepToString(array));
-        int maxColumn = maxCol(array, N, M);
-        int[][] newArray = delMaxCol(N, M, maxColumn, array);
-        System.out.println(Arrays.deepToString(newArray));
+        System.out.println("Исходная матрица:");
+        printArray(array);
+        int maxColumn = maxCol(array);
+        array = delMaxCol(array, maxColumn);
+        System.out.println("Новая матрица:");
+        printArray(array);
     }
 }
